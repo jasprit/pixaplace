94cd02d45713249b2b05cdc4f51395c8a322f842
JaspreetS-FOX <jaspreet.singh1@fox.com>
2025-10-24 20:14:06 -0700

    - icons added!

--- composeApp/src/commonMain/kotlin/com/pixaplace/ui/components/BottomNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/com/pixaplace/ui/components/BottomNavigationBar.kt
@@ -1,5 +1,6 @@
 package com.pixaplace.ui.components
 
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -17,17 +18,19 @@ fun BottomNavigationBar(
 
         items.forEach { item ->
             val selected = currentDestination?.route == item.route
-            NavigationBarItem(
-                icon = { },
-                label = { Text(item.label) },
-                selected = selected,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.startDestinationId) { saveState = true }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                })
+            NavigationBarItem(icon = {
+                item.icon?.let {
+                    Icon(
+                        imageVector = it, contentDescription = item.label
+                    )
+                }
+            }, label = { Text(item.label) }, selected = selected, onClick = {
+                navController.navigate(item.route) {
+                    popUpTo(navController.graph.startDestinationId) { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            })
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/pixaplace/ui/navigation/SideBarMenu.kt
+++ composeApp/src/commonMain/kotlin/com/pixaplace/ui/navigation/SideBarMenu.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -70,11 +74,24 @@ fun SidebarMenu(
                         .padding(vertical = 16.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(
-                        text = item.label,
-                        color = textColor,
-                        style = androidx.compose.material3.MaterialTheme.typography.bodyLarge
-                    )
+                    Row {
+                        item.icon?.let {
+                            Icon(
+                                imageVector = it,
+                                contentDescription = item.label,
+                                tint = textColor, // optional: to match your text color or theme
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            text = item.label,
+                            color = textColor,
+                            style = androidx.compose.material3.MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
             }
         }
